268fe87c59d3d9a0b3e4bceb6f9e8e7ab0c28319
Moyq5 <mo_yq5@163.com>
2019-07-01 18:56:10 +0800

    将matrix Template模板创建基于vue的管理后台模板项目

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="main-wrapper">
+      <Topbar></Topbar>
+      <Sidebar></Sidebar>
+      <ListPage></ListPage>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Topbar from "./components/Topbar";
+import Sidebar from "./components/Sidebar";
+import ListPage from "./components/ListPage";
 
 export default {
-  name: 'app',
+  name: "App",
   components: {
-    HelloWorld
+    Topbar,
+    Sidebar,
+    ListPage
   }
-}
+};
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
